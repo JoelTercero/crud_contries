bbccbb118f2d603a11ffe27afc4166132044d31e
Joel Tercero <joel_3p@hotmail.com>
2021-09-30 01:50:27 -0500

    Initial commit

---
+++ src/main/java/com/country/crud/CrudContriesApplication.java
@@ -0,0 +1,13 @@
+package com.country.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudContriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudContriesApplication.class, args);
+	}
+
+}
